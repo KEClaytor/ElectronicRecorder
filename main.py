20063abf4ba6fd5e5ca804a3e31a8f2b49670c41
Kevin Claytor <KE.Claytor@gmail.com>
2021-09-19 14:30:51 -0400

    Including MIDI notes and rudimentary lookup.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import time
 import board
 import busio
+import digitalio
+
 import adafruit_midi
 
 from adafruit_midi.control_change import ControlChange
@@ -25,55 +27,94 @@ rest = note_hold / 5
 print("MIDI Out demo")
 print("Default output channel:", midi.out_channel)
 
+rp_keys = list(
+    map(
+        digitalio.DigitalInOut,
+        [
+            board.D25,
+            board.D13,
+            board.D12,
+            board.D11,
+            board.D10,
+            board.D9,
+            board.D6,
+            board.D5,
+        ]
+    )
+)
+keys = rp_keys
+
+for k in keys:
+    k.switch_to_input(pull=digitalio.Pull.UP)
+
+notes_soprano = {
+    255: 60,
+    254: 62,
+    252: 64,
+    251: 65,
+    246: 66,
+    240: 67,
+    236: 68,
+    224: 69,
+    216: 70,
+    192: 71,
+    160: 72,
+    96: 81,
+    32: 74,
+    62: 75,
+    124: 76,
+    122: 77,
+    116: 78,
+    112: 79,
+    104: 80,
+    102: 82,
+    108: 83,
+    76: 84,
+    90: 86
+}
+notes = notes_soprano
+
+def poll(keys):
+    """Poll the keys to determine the note value.
+    """
+    value = 0
+    for ii, k in enumerate(keys):
+        # Buttons are pull-up, pressed = off
+        if not k.value:
+            value += 2**(7 - ii)
+    return value
+
+def fingering(keys):
+    str = ""
+    for ii, k in enumerate(keys):
+        # Buttons are pull-up, pressed = off
+        if not k.value:
+            str += "|x"
+        else:
+            str += "|o"
+    return str
+
+old_v = 0
+old_note = 0
+midi.send(ControlChange(64, 0)) # sustain CC
+midi.send(ControlChange(1, 0))  # modulation CC
 while True:
-    midi.send(ControlChange(64, 0))  # sustain CC
-    # midi.send(ControlChange(1, 0))  # modulation CC
-
-    for ii in range(48, 55):
-        midi.send(NoteOn(ii, 100))
-        time.sleep(note_hold)
-        midi.send(NoteOff(ii, 0))
-        time.sleep(rest)
-
-    midi.send(NoteOn(48, 20))  # play note
-    time.sleep(note_hold)  # hold note
-    midi.send(NoteOff(48, 0))  # release note
-    time.sleep(rest)  # rest
-
-    midi.send(NoteOn(55, 40))
-    time.sleep(note_hold)
-    midi.send(NoteOff(55, 0))
-    time.sleep(rest)
-
-    midi.send(NoteOn(51, 60))
-    time.sleep(note_hold)
-    midi.send(NoteOff(51, 0))
-    time.sleep(rest)
-
-    midi.send(NoteOn(58, 80))
-    time.sleep(note_hold)
-    midi.send(NoteOff(58, 0))
-    time.sleep(rest)
-
-    midi.send(ControlChange(64, 32))
-    # midi.send(ControlChange(1, 127))
-
-    midi.send(NoteOn(48, 20))  # play note
-    time.sleep(note_hold)  # hold note
-    midi.send(NoteOff(48, 0))  # release note
-    time.sleep(rest)  # rest
-
-    midi.send(NoteOn(55, 40))
-    time.sleep(note_hold)
-    midi.send(NoteOff(55, 0))
-    time.sleep(rest)
-
-    midi.send(NoteOn(51, 60))
-    time.sleep(note_hold)
-    midi.send(NoteOff(51, 0))
-    time.sleep(rest)
-
-    midi.send(NoteOn(50, 80))
-    time.sleep(note_hold)
-    midi.send(NoteOff(50, 0))
-    time.sleep(rest)
+    # Poll the new value
+    new_v = poll(keys)
+    # Debug: print the fingering
+    # print("%4s" % new_v, fingering(keys))
+    # On a note change play the new note
+    if not (new_v == old_v):
+        # Turn off the old note
+        midi.send(NoteOff(old_note, 100))
+        try:
+            new_note = notes[new_v]
+        except KeyError as ex:
+            new_note = 0
+            print("Fingering not found!")
+        # Turn on the new note
+        midi.send(NoteOn(new_note, 100))
+        # store the new notes as the old notes
+        old_v = new_v
+        old_note = new_note
+    # time.sleep(0.1)
